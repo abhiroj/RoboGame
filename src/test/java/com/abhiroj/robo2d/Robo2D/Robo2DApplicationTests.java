3969496c67676efb940fae6ad2f12edb7ef4392f
abhiroj <abhiroj95@gmail.com>
2019-05-24 19:52:28 +0530

    assert test

---
+++ src/test/java/com/abhiroj/robo2d/Robo2D/Robo2DApplicationTests.java
@@ -0,0 +1,16 @@
+package com.abhiroj.robo2d.Robo2D;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Robo2DApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
