54a94820b2c34d746770482f219f01b0f33b2d26
Abhiroj <abhiroj95@gmail.com>
2019-05-27 09:53:44 +0530

    add core and service module

---
+++ service/src/main/java/com/playground/robo2d/service/App.java
@@ -0,0 +1,13 @@
+package com.playground.robo2d.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
--- src/main/java/com/playground/robo2d/Application.java
+++
@@ -1,8 +0,0 @@
-package com.abhiroj.robo2d.Robo2D;
-
-public class Application {
-	
-	public static void main(String args[]){
-		
-	}
-}
