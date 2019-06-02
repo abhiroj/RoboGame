c2c903cbe5893b1a4f767c818a0475f5f9123292
Abhiroj <abhiroj95@gmail.com>
2019-06-03 00:58:51 +0530

    make rover-implementation multi-threaded

--- core/src/main/java/core/game/RoverImpl.java
+++ core/src/main/java/core/game/RoverImpl.java
@@ -2,15 +2,21 @@ package core.src.main.java.core.game;
 
 import core.src.main.java.core.exception.AppException;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 class RoverImpl implements Rover, Runnable {
 
     private Mediator mediator;
     private Coordinate coordinate;
     private final int id;
     private final int INTERVAL_IN_MILLIS = 2000;
+    private List<Map<String, Object>> repository;
 
     RoverImpl(int id) {
         this.id = id;
+        repository = new ArrayList<>();
     }
 
     @Override
@@ -20,7 +26,7 @@ class RoverImpl implements Rover, Runnable {
 
     @Override
     public void move() {
-        
+        new Thread(this, "Rover " + id).start();
     }
 
     @Override
@@ -30,7 +36,7 @@ class RoverImpl implements Rover, Runnable {
 
     @Override
     public void setStartingCoordinate(Coordinate coordinate) {
-        if (this.coordinate == null) {
+        if (this.coordinate != null) {
             throw new AppException("can not set coordinates while in operation");
         }
         this.coordinate = coordinate;
@@ -54,6 +60,18 @@ class RoverImpl implements Rover, Runnable {
 
     @Override
     public void run() {
-
+        mediator.shouldMove(coordinate);
+        System.out.println(Thread.currentThread().getName() + " successfully deployed!");
+        while (true) {
+            repository.add(mediator.collect(coordinate));
+            System.out.println("Collection made at " + coordinate.toString());
+            try {
+                Thread.sleep(INTERVAL_IN_MILLIS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            coordinate = new Coordinate(coordinate.getX() + 1, coordinate.getY());
+            mediator.shouldMove(coordinate);
+        }
     }
 }
