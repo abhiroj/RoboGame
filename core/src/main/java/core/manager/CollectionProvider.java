d0033e35e82c2f731434a87765d9fad9d0866d7a
Abhiroj <abhiroj95@gmail.com>
2019-06-21 09:21:55 +0530

    add functionality to game manager + api comments on CollectionProvider

--- core/src/main/java/core/manager/CollectionProvider.java
+++ core/src/main/java/core/manager/CollectionProvider.java
@@ -4,10 +4,16 @@ package core.manager;
 import core.elements.coordinate.Coordinate;
 
 /**
- * Classes implementing this plays the role of collecting properties from playground present at the coordinates
+ * Classes implementing this plays the role of collecting properties from playground present at the coordinates.
+ * To be used by a rover to trigger a collection
  */
 public interface CollectionProvider {
 
-    void collect(Coordinate c);
+    /**
+     * consumer intends to collect at this coordinate.
+     *
+     * @param coordinate
+     */
+    void collect(Coordinate coordinate);
 
 }
--- core/src/main/java/core/manager/MovementProvider.java
+++ core/src/main/java/core/manager/MovementProvider.java
@@ -11,7 +11,7 @@ public interface MovementProvider {
 
     /**
      * Movement Provider tells next valid coordinate to move on, given current coordinate
-     * throws runtime exception if not valid coordinate is found.
+     * throws <code>NoCoordinateFoundException</code> if no valid coordinate is found for next move.
      *
      * @param coordinate
      * @return
@@ -21,6 +21,8 @@ public interface MovementProvider {
     /**
      * determines next move using direction strategy.
      * Direction strategy or diffCoordinates provides the directional change consumer provides.
+     * throws <code>NoCoordinateFoundException</code> if no valid coordinate is found for next move.
+     * Currently, throws not implemented exception.
      *
      * @return
      */
