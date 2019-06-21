f87a0f71e8945e53c1c27516b95d020a96beac38
Abhiroj <abhiroj95@gmail.com>
2019-06-21 14:10:05 +0530

    add test

---
+++ core/src/test/java/core/elements/coordinate/CoordinateImplTest.java
@@ -0,0 +1,13 @@
+package core.elements.coordinate;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.Map;
+
+@RunWith(JUnit4.class)
+public class CoordinateImplTest {
+
+    private Coordinate coordinate;
+    private Map<AxisType, Integer> axisTypeIntegerMap;
+}
